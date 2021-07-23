0b7ea25433a1d0ca55be241defe026655d7c50d9
Robson Carvalho de Souza <82351564+Robson-Carvalho@users.noreply.github.com>
2021-07-23 14:00:04 -0300

    OTAKUFLIX 1.0
    Projeto finalizado, mas ainda depende de melhorias na resposividade e formatação do código, os quais seram tratados futuramente.

---
+++ Js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:14,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:8
+        }
+    }
+});
